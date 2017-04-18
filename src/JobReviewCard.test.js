1ac55deb53e448cf2aa63cc5225218b7ecb43434
Krista Calderon <kamsci@gmail.com>
2017-04-18 15:33:15 -0700

    Reinstalled JEst v.18 to replace v.19 in order for testing to run; created JobReviewCard render test and JobReviewCard editMode test - both pass

--- src/JobReviewCard.test.js
+++ src/JobReviewCard.test.js
@@ -1,8 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import JobReviewCard from './JobReviewCard';
+import DescriptionReviewContainer from './DescriptionReviewContainer';
+import JobForm from './JobForm';
+import renderer from 'react-test-renderer';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<JobReviewCard />, div);
+});
+
+test('JobReviewCard changes when editMode toggles', () => {
+  const component = renderer.create(
+    <JobReviewCard />
+  );
+
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+
+  // manually trigger editMode
+  tree.props.editMode = false;
+  // re-rendering
+  tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+
+  // manually trigger the callback
+  tree.props.editMode = true;
+  // re-rendering
+  tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
 });
